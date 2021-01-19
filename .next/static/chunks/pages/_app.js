16521e1e15b0f43a2c751ec7e68d87ea12eb0fb3
codepurse <47669262+codepurse@users.noreply.github.com>
2021-01-19 14:31:26 +0800

    Update

--- .next/static/chunks/pages/_app.js
+++ .next/static/chunks/pages/_app.js
@@ -9057,7 +9057,7 @@ module.exports = function(module) {
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! next-client-pages-loader?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2F_app&absolutePagePath=next%2Fdist%2Fpages%2F_app!./");
-module.exports = __webpack_require__(/*! C:\Users\Alfon\Documents\Work Projects\Github\AAA\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
+module.exports = __webpack_require__(/*! C:\Users\Alfon\Documents\Work Projects\Github\AAAEquities\node_modules\next\dist\client\router.js */"./node_modules/next/dist/client/router.js");
 
 
 /***/ })
--- pages/index.js
+++ pages/index.js
@@ -40,6 +40,19 @@ function index() {
 
   function nextForm() {
     if ($(".divForm1").css("display") == "block") {
+      if ($(window).width() < 501) {
+        $(".imgRight").css("width", "50%");
+        $(".colImage").addClass("col-sm-4 col-4");
+        $(".box").css("margin-top", "50px");
+        $(".circle2").addClass("circleactivemobile");
+        $(".circle1").addClass("circlenotactivemobile");
+      } else {
+        $(".imgRight").css("width", "auto");
+        $(".imgCircle2").css("width", "25px");
+        $(".imgCircle1").css("width", "15px");
+        $(".circle2").removeClass("circlenotactive");
+        $(".circle1").addClass("circlenotactive");
+      }
       $(".colLeft").removeClass("col-lg-6");
       $(".colLeft").addClass("col-lg-8");
       $(".colRight").removeClass("col-lg-6");
@@ -50,34 +63,48 @@ function index() {
       $(".imgRight").addClass("animate__animated animate__zoomIn");
       $(".imgRight").attr("src", "Image/form2.svg");
       $(".pForm1").fadeIn(200);
-      $(".imgCircle2").css("width", "25px");
-      $(".imgCircle1").css("width", "15px");
-      $(".circle2").removeClass("circlenotactive");
-      $(".circle1").addClass("circlenotactive");
+
+      $(window).scrollTop(0);
+      $(".colBack").show();
     } else if ($(".divForm2").css("display") == "block") {
       $(".divForm3").fadeIn(200);
       $(".divForm2").hide();
       $(".box").css("height", "450px");
+      $(".box").css("margin-top", "-40px");
+      $(window).scrollTop(0);
     } else if ($(".divForm3").css("display") == "block") {
+      if ($(window).width() < 501) {
+        $(".imgRight").css("width", "50%");
+        $(".colImage").addClass("col-sm-4 col-4");
+        $(".box").css("margin-top", "50px");
+        $(".circle3").addClass("circleactivemobile");
+        $(".circle2").addClass("circlenotactivemobile");
+      } else {
+        $(".imgCircle3").css("width", "25px");
+        $(".imgCircle2").css("width", "15px");
+        $(".imgRight").addClass("animate__animated animate__zoomIn");
+        $(".imgRight").attr("src", "Image/form3.svg");
+        $(".circle3").removeClass("circlenotactive");
+        $(".circle2").addClass("circlenotactive");
+      }
       $(".divForm4").fadeIn(200);
       $(".divForm3").hide();
       $(".box").css("height", "550px");
-      $(".imgCircle3").css("width", "25px");
-      $(".imgCircle2").css("width", "15px");
-      $(".imgRight").addClass("animate__animated animate__zoomIn");
-      $(".imgRight").attr("src", "Image/form3.svg");
-      $(".circle3").removeClass("circlenotactive");
-      $(".circle2").addClass("circlenotactive");
+
       $(".pForm").hide();
+      $(".box").css("margin-top", "10px");
+      $(window).scrollTop(0);
       $(".pForm2").fadeIn(200);
     } else if ($(".divForm4").css("display") == "block") {
       $(".divForm5").fadeIn(200);
       $(".divForm4").hide();
       $(".box").css("height", "550px");
+      $(window).scrollTop(0);
     } else if ($(".divForm5").css("display") == "block") {
       $(".divForm6").fadeIn(200);
       $(".divForm5").hide();
       $(".box").css("height", "550px");
+      $(window).scrollTop(0);
     } else if ($(".divForm6").css("display") == "block") {
       $(".colLeft").removeClass("col-lg-8");
       $(".colLeft").addClass("col-lg-6");
@@ -90,32 +117,67 @@ function index() {
       $(".imgRight").attr("src", "Image/form7.svg");
       $(".pForm").hide();
       $(".pForm7").fadeIn(200);
+      $(window).scrollTop(0);
     } else if ($(".divForm7").css("display") == "block") {
+      console.log("ito yon");
+      if ($(window).width() < 501) {
+        $(".imgRight").css("width", "30%");
+        $(".colImage").removeClass("col-sm-4 col-4");
+        $(".pForm7").addClass("pImportant");
+        $(".colText").removeClass("col-sm-8 col-8");
+      } else {
+        $(".imgRight").css("width", "auto");
+      }
       $(".divForm8").fadeIn(200);
       $(".divForm7").hide();
       $(".box").css("height", "350px");
+      $(window).scrollTop(0);
     } else if ($(".divForm8").css("display") == "block") {
       $(".divForm9").fadeIn(200);
       $(".divForm8").hide();
       $(".box").css("height", "350px");
+      $(window).scrollTop(0);
     } else if ($(".divForm9").css("display") == "block") {
       $(".divForm10").fadeIn(200);
       $(".divForm9").hide();
       $(".box").css("height", "450px");
+      $(window).scrollTop(0);
     } else if ($(".divForm10").css("display") == "block") {
       $(".divForm11").fadeIn(200);
       $(".divForm10").hide();
       $(".box").css("height", "300px");
-    }else if ($(".divForm11").css("display") == "block") {
-      $(".pBox").css("font-size","2.2rem");
+      $(window).scrollTop(0);
+      if ($(window).width() < 501) {
+        $(".imgRight").css("width", "70%");
+        $(".colImage").removeClass("col-sm-4 col-4");
+      } else {
+        $(".imgRight").css("width", "auto");
+      }
+      $(".imgRight").css("width", "80%");
+      $(".imgRight").addClass("animate__animated animate__zoomIn");
+      $(".imgRight").attr("src", "Image/Phone1.png");
+      $(".pForm").hide();
+    } else if ($(".divForm11").css("display") == "block") {
+      $(".pBox").css("font-size", "2.2rem");
       $(".divForm12").fadeIn(200);
       $(".divForm11").hide();
       $(".box").css("height", "315px");
+      $(window).scrollTop(0);
     }
   }
 
   function prevForm() {
     if ($(".divForm2").css("display") == "block") {
+      if ($(window).width() < 501) {
+        $(".imgRight").css("width", "70%");
+        $(".colImage").removeClass("col-sm-4 col-4");
+
+        $(".circle1").addClass("circleactivemobile");
+
+        $(".circle2").removeClass("circleactivemobile");
+      } else {
+        $(".imgRight").css("width", "auto");
+      }
       $(".colLeft").removeClass("col-lg-8");
       $(".colLeft").addClass("col-lg-6");
       $(".colRight").removeClass("col-lg-4");
@@ -124,26 +186,35 @@ function index() {
       $(".divForm2").hide();
       $(".box").css("height", "350px");
       $(".imgRight").addClass("animate__animated animate__zoomIn");
-      $(".imgRight").attr("src", "Image/Phone.png");
+      $(".imgRight").attr("src", "Image/Phone1.png");
       $(".pForm").hide();
       $(".imgCircle1").css("width", "25px");
       $(".imgCircle2").css("width", "15px");
       $(".circle1").removeClass("circlenotactive");
       $(".circle2").addClass("circlenotactive");
+      $(".colBack").hide();
     } else if ($(".divForm3").css("display") == "block") {
       $(".divForm2").fadeIn(200);
       $(".divForm3").hide();
       $(".box").css("height", "500px");
+      $(".box").css("margin-top", "30px");
     } else if ($(".divForm4").css("display") == "block") {
+      if ($(window).width() < 501) {
+        $(".circle2").addClass("circleactivemobile");
+
+        $(".circle3").removeClass("circleactivemobile");
+      } else {
+        $(".imgCircle2").css("width", "25px");
+        $(".imgCircle3").css("width", "15px");
+        $(".imgRight").addClass("animate__animated animate__zoomIn");
+        $(".imgRight").attr("src", "Image/form2.svg");
+        $(".circle2").removeClass("circlenotactive");
+        $(".circle3").addClass("circlenotactive");
+      }
       $(".divForm3").fadeIn(200);
       $(".divForm4").hide();
       $(".box").css("height", "450px");
-      $(".imgCircle2").css("width", "25px");
-      $(".imgCircle3").css("width", "15px");
-      $(".imgRight").addClass("animate__animated animate__zoomIn");
-      $(".imgRight").attr("src", "Image/form2.svg");
-      $(".circle2").removeClass("circlenotactive");
-      $(".circle3").addClass("circlenotactive");
+
       $(".pForm2").hide();
       $(".pForm1").fadeIn(200);
     } else if ($(".divForm5").css("display") == "block") {
@@ -153,6 +224,14 @@ function index() {
       $(".divForm5").fadeIn(200);
       $(".divForm6").hide();
     } else if ($(".divForm7").css("display") == "block") {
+      console.log("naay");
+      if ($(window).width() < 501) {
+        $(".imgRight").css("width", "50%");
+        $(".colImage").addClass("col-sm-4 col-4");
+        $(".colText").addClass("col-sm-8 col-8");
+      } else {
+        $(".imgRight").css("width", "auto");
+      }
       $(".colLeft").removeClass("col-lg-6");
       $(".colLeft").addClass("col-lg-8");
       $(".colRight").removeClass("col-lg-6");
@@ -171,12 +250,20 @@ function index() {
     } else if ($(".divForm9").css("display") == "block") {
       $(".divForm8").fadeIn(200);
       $(".divForm9").hide();
-      $(".box").css("height", "450px");
+      $(".box").css("height", "350px");
     } else if ($(".divForm10").css("display") == "block") {
       $(".divForm9").fadeIn(200);
       $(".divForm10").hide();
       $(".box").css("height", "350px");
     } else if ($(".divForm11").css("display") == "block") {
+      if ($(window).width() < 501) {
+        $(".imgRight").css("width", "50%");
+      } else {
+        $(".imgRight").css("width", "auto");
+      }
+      $(".imgRight").addClass("animate__animated animate__zoomIn");
+      $(".imgRight").attr("src", "Image/form7.svg");
+      $(".pForm7").show();
       $(".divForm10").fadeIn(200);
       $(".divForm11").hide();
       $(".box").css("height", "450px");
@@ -203,9 +290,9 @@ function index() {
       </Head>
       <Header />
       <Navbar />
-      <div className="container h-100">
+      <div className="container h-100 conMain">
         <div className="row h-100 align-items-center">
-          <div className="col-lg-6 colLeft">
+          <div className="col-lg-6 colLeft order-2 order-lg-1">
             <div className="box">
               <div className="boxLine"></div>
               <div className="boxCircle">
@@ -381,7 +468,7 @@ function index() {
                   style={{ marginTop: "15px" }}
                 >
                   <div className="col-lg-12">
-                    <div className="form-inline">
+                    <div className="form-inline inlineDate">
                       <p className="pBoxTitle">Birth Date</p>
                       <input
                         type="text"
@@ -769,23 +856,25 @@ function index() {
                 <p className="pBoxTitle" style={{ marginBottom: "18px" }}>
                   Are you associated with another broker or dealer?
                 </p>
-                <label htmlFor="rdo-11" className="btn-radio">
-                  <input type="radio" id="rdo-11" name="radio-grpDealer" />
-                  <svg width="20px" height="20px" viewBox="0 0 20 20">
-                    <circle cx={10} cy={10} r={9} />
-                    <path
-                      d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z"
-                      className="inner"
-                    />
-                    <path
-                      d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z"
-                      className="outer"
-                    />
-                  </svg>
-                  <span className="pRadio">
-                    Yes, if so state the name of the company
-                  </span>
-                </label>
+                <div className="radio">
+                  <label htmlFor="rdo-11" className="btn-radio">
+                    <input type="radio" id="rdo-11" name="radio-grpDealer" />
+                    <svg width="20px" height="20px" viewBox="0 0 20 20">
+                      <circle cx={10} cy={10} r={9} />
+                      <path
+                        d="M10,7 C8.34314575,7 7,8.34314575 7,10 C7,11.6568542 8.34314575,13 10,13 C11.6568542,13 13,11.6568542 13,10 C13,8.34314575 11.6568542,7 10,7 Z"
+                        className="inner"
+                      />
+                      <path
+                        d="M10,1 L10,1 L10,1 C14.9705627,1 19,5.02943725 19,10 L19,10 L19,10 C19,14.9705627 14.9705627,19 10,19 L10,19 L10,19 C5.02943725,19 1,14.9705627 1,10 L1,10 L1,10 C1,5.02943725 5.02943725,1 10,1 L10,1 Z"
+                        className="outer"
+                      />
+                    </svg>
+                    <span className="pRadio">
+                      Yes, if so state the name of the company
+                    </span>
+                  </label>
+                </div>
                 <div
                   className="row"
                   style={{ marginTop: "10px", marginBottom: "10px" }}
@@ -794,7 +883,7 @@ function index() {
                     <input
                       type="text"
                       className="txtBox"
-                      placeholder="Account number"
+                      placeholder="Company name"
                     ></input>
                   </div>
                 </div>
@@ -991,10 +1080,12 @@ function index() {
                 </div>
               </div>
               <div className="row" style={{ marginTop: "15px" }}>
-                <div className="col-lg-12">
+                <div className="col-lg-4 colBack col-sm-6 col-6">
                   <button className="btnBack" onClick={prevForm}>
                     BACK
                   </button>
+                </div>
+                <div className="col-lg-4 col-sm-6 col-6">
                   <button className="btnNext" onClick={nextForm}>
                     NEXT
                   </button>
@@ -1002,23 +1093,31 @@ function index() {
               </div>
             </div>
           </div>
-          <div className="col-lg-6 colRight">
-            <img
-              src="Image/Phone.png"
-              className="img-fluid imgRight mx-auto d-flex animate__faster"
-            ></img>
-            <p className="pForm pForm1">
-              When <b>resistance</b> levels are broken, a breakout may occur.
-              Buy breakouts automatically with Conditional Orders.
-            </p>
-            <p className="pForm pForm2">
-              When <b>support</b> levels are broken, a breakout may occur. Set
-              automatic stops with Conditional orders to protect your capital.
-            </p>
-            <p className="pForm pForm7">
-              <b>Margin</b> increases your buying power by borrowing funds. To
-              use margin, upgrade your account to a AAA Turbo Margin Account.
-            </p>
+          <div className="col-lg-6 colRight order-1 order-lg-2">
+            <div className="row align-items-center">
+              <div className="col-lg-12 colImage">
+                <img
+                  src="Image/Phone1.png"
+                  className="img-fluid imgRight mx-auto d-flex animate__faster"
+                ></img>
+              </div>
+              <div className="col-lg-12 col-sm-8 col-8 colText">
+                <p className="pForm pForm1">
+                  When <b>resistance</b> levels are broken, a breakout may
+                  occur. Buy breakouts automatically with Conditional Orders.
+                </p>
+                <p className="pForm pForm2">
+                  When <b>support</b> levels are broken, a breakout may occur.
+                  Set automatic stops with Conditional orders to protect your
+                  capital.
+                </p>
+                <p className="pForm pForm7">
+                  <b>Margin</b> increases your buying power by borrowing funds.
+                  To use margin, upgrade your account to a AAA Turbo Margin
+                  Account.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
