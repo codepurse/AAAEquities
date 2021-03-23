46e0440f63c83acaeba3039cf40a6ec6cc7dda68
codepurse <47669262+codepurse@users.noreply.github.com>
2021-03-23 18:24:24 +0800

    update

--- pages/upgrade/form.js
+++ pages/upgrade/form.js
@@ -23,6 +23,13 @@ function form() {
     console.log(token);
   }, []);
 
+  function removeClientpic() {
+    setClient("");
+    setClientpic("");
+    $(".inputFile1").val("");
+    $(".divUploadclient").hide();
+  }
+
   function numOnly(event) {
     let value = event.currentTarget.value;
     let numbers = value.replace(/[^0-9]/g, "");
@@ -139,18 +146,24 @@ function form() {
         </div>
       );
       $(".txtMargin").css("border", "1px solid red");
-    } else if ( !$("#cbx1").is(":checked")) {
+    } else if (!$("#cbx1").is(":checked")) {
       swal(
         <div style={{ width: "450px", padding: "20px 8px" }}>
           <div className="container">
             <div className="row">
-              <div className="col-lg-2 col-md-2 col-sm-2 col-2" style={{ padding: "0px" }}>
+              <div
+                className="col-lg-2 col-md-2 col-sm-2 col-2"
+                style={{ padding: "0px" }}
+              >
                 <img
                   src="../Image/error.png"
                   style={{ width: "32px", marginTop: "0px" }}
                 ></img>
               </div>
-              <div className="col-lg-10 col-sm-10 col-md-10 col-10" style={{ padding: "0px" }}>
+              <div
+                className="col-lg-10 col-sm-10 col-md-10 col-10"
+                style={{ padding: "0px" }}
+              >
                 <p className="pError">Something went wrong</p>
                 <p className="pErrorSub">
                   Please agree to our terms and conditions and privacy policy.
@@ -163,9 +176,6 @@ function form() {
     } else {
       $("#modalVerify").modal("hide");
 
-    
-
-
       const options = {
         headers: {
           Accept: "application/json, text/plain, */*",
@@ -175,62 +185,74 @@ function form() {
         },
       };
       let formdata = new FormData();
-      formdata.append("username",username);
+      formdata.append("username", username);
       formdata.append("email", email);
       formdata.append("margin_amount_requested", marginamount);
-      formdata.append("token", formtoken  );
+      formdata.append("token", formtoken);
       formdata.append("client_signature_image", client, client.name);
 
-      
-      const api_upgrade = appglobal.api.base_api + appglobal.api.upgrade_accounts;
+      const api_upgrade =
+        appglobal.api.base_api + appglobal.api.upgrade_accounts;
 
       axios
-      .post(api_upgrade, formdata, options)
-      .then((result) => {
-        console.log(result);
-        $("#exampleModalCenter").modal("hide");
-        $("#modalVerify").modal("hide");
-        swal(
-          <div style={{ width: "450px", padding: "20px 8px" }}>
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col-lg-2 col-sm-2 col-2 col-md-2" style={{ padding: "0px" }}>
-                  <img
-                    src="Image/check.png"
-                    style={{ width: "32px", marginTop: "0px" }}
-                  ></img>
-                </div>
-                <div className="col-lg-10 col-sm-10 col-10 col-md-10" style={{ padding: "0px" }}>
-                  <p className="pError">Success</p>
-                  <p className="pErrorSub">Account succesfully upgraded.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        );
-      })
-      .catch((err) => {
-        console.log(err.response);
-        $("#modalVerify").modal("hide");
-        swal(
-          <div style={{ width: "450px", padding: "20px 8px" }}>
-            <div className="container-fluid">
-              <div className="row">
-                <div className="col-lg-2 col-sm-2 col-2 col-md-2" style={{ padding: "0px" }}>
-                  <img
-                    src="../Image/error.png"
-                    style={{ width: "32px", marginTop: "0px" }}
-                  ></img>
-                </div>
-                <div className="col-lg-10 col-sm-10 col-10 col-md-10" style={{ padding: "0px" }}>
-                  <p className="pError">Something went wrong</p>
-                  <p className="pErrorSub">{err.response.data.message}</p>
+        .post(api_upgrade, formdata, options)
+        .then((result) => {
+          console.log(result);
+          $("#exampleModalCenter").modal("hide");
+          $("#modalVerify").modal("hide");
+          swal(
+            <div style={{ width: "450px", padding: "20px 8px" }}>
+              <div className="container-fluid">
+                <div className="row">
+                  <div
+                    className="col-lg-2 col-sm-2 col-2 col-md-2"
+                    style={{ padding: "0px" }}
+                  >
+                    <img
+                      src="Image/check.png"
+                      style={{ width: "32px", marginTop: "0px" }}
+                    ></img>
+                  </div>
+                  <div
+                    className="col-lg-10 col-sm-10 col-10 col-md-10"
+                    style={{ padding: "0px" }}
+                  >
+                    <p className="pError">Success</p>
+                    <p className="pErrorSub">Account succesfully upgraded.</p>
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      });
+          );
+        })
+        .catch((err) => {
+          console.log(err.response);
+          $("#modalVerify").modal("hide");
+          swal(
+            <div style={{ width: "450px", padding: "20px 8px" }}>
+              <div className="container-fluid">
+                <div className="row">
+                  <div
+                    className="col-lg-2 col-sm-2 col-2 col-md-2"
+                    style={{ padding: "0px" }}
+                  >
+                    <img
+                      src="../Image/error.png"
+                      style={{ width: "32px", marginTop: "0px" }}
+                    ></img>
+                  </div>
+                  <div
+                    className="col-lg-10 col-sm-10 col-10 col-md-10"
+                    style={{ padding: "0px" }}
+                  >
+                    <p className="pError">Something went wrong</p>
+                    <p className="pErrorSub">{err.response.data.message}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        });
     }
   }
 
@@ -269,6 +291,12 @@ function form() {
       $(".pUploadclient").text(file.name);
       setClientpic(file.name);
       setClient(file);
+      reader.onloadend = function () {
+        $(".imgClientpic").attr("src", reader.result);
+      };
+      reader.readAsDataURL(file);
+      $(".divClient").attr("style", "2px solid #9098A9");
+      $(".divUploadclient").show();
     }
   }
 
@@ -337,29 +365,61 @@ function form() {
                       <p className="pUpload">Max file size: 4mb</p>
                       <p className="pUpload">Accepted file type: Jpg and Png</p>
                     </div>
-                   <div style = {{marginTop: "10px"}}>
-                   <input
-                      className="inp-cbx"
-                      id="cbx1"
-                      type="checkbox"
-                      style={{ display: "none" }}
-                    />
-                    <label className="cbx" htmlFor="cbx1">
-                      <span>
-                        <svg width="12px" height="10px" viewBox="0 0 12 10">
-                          <polyline points="1.5 6 4.5 9 10.5 1" />
-                        </svg>
-                      </span>
-                    </label>
-                    <a
-                      target="_blank"
-                      href="https://aaa-equities.com.ph/privacy-policy/"
-                    >
-                      <span className="pCheck">
-                        I have read the margin trading agreement.
-                      </span>
-                    </a>
-                   </div>
+                    <div className="container divUploadclient">
+                      <div className="row">
+                        <div className="col-lg-2 col-sm-2 col-2">
+                          <img
+                            src="../Image/jpg.png"
+                            className="img-fluid mx-auto d-flex"
+                            style={{ width: "45px" }}
+                          ></img>
+                        </div>
+                        <div
+                          className="col-lg-10 col-sm-10 col-10"
+                          style={{ position: "relative" }}
+                        >
+                          <img
+                            src="../Image/trash-can.png"
+                            className="imgTrash"
+                            onClick={removeClientpic}
+                          ></img>
+                          <p className="pUploadname pUploadclient">
+                            Sample file.png
+                          </p>
+                          <p
+                            className="pView"
+                            data-toggle="modal"
+                            data-target="#exampleModalCenter1"
+                         
+                          >
+                            View image
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div style={{ marginTop: "10px" }}>
+                      <input
+                        className="inp-cbx"
+                        id="cbx1"
+                        type="checkbox"
+                        style={{ display: "none" }}
+                      />
+                      <label className="cbx" htmlFor="cbx1">
+                        <span>
+                          <svg width="12px" height="10px" viewBox="0 0 12 10">
+                            <polyline points="1.5 6 4.5 9 10.5 1" />
+                          </svg>
+                        </span>
+                      </label>
+                      <a
+                        target="_blank"
+                        href="https://aaa-equities.com.ph/privacy-policy/"
+                      >
+                        <span className="pCheck">
+                          I have read the margin trading agreement.
+                        </span>
+                      </a>
+                    </div>
                     <div
                       className="mx-auto d-flex text-center"
                       style={{ width: "100px" }}
@@ -416,47 +476,75 @@ function form() {
         </div>
       </div>
 
-        <div
-      className="modal "
-      id="modalVerify"
-      data-backdrop="static"
-      data-keyboard="false"
-      tabIndex={-1}
-      role="dialog"
-      aria-labelledby="exampleModalCenterTitle"
-      aria-hidden="true"
-    >
-      <div className="modal-dialog modal-dialog-centered" role="document">
-        <div className="modal-content modalBorder">
-          <div className="modal-body">
-            <div className="container">
-              <div className="row  ">
-                <div className="col-lg-12 ">
-                  <div className="colVerify">
-                    <div className="Box">
-                      <span>
-                        <span></span>
-                      </span>
+      <div
+        className="modal "
+        id="modalVerify"
+        data-backdrop="static"
+        data-keyboard="false"
+        tabIndex={-1}
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div className="modal-dialog modal-dialog-centered" role="document">
+          <div className="modal-content modalBorder">
+            <div className="modal-body">
+              <div className="container">
+                <div className="row  ">
+                  <div className="col-lg-12 ">
+                    <div className="colVerify">
+                      <div className="Box">
+                        <span>
+                          <span></span>
+                        </span>
+                      </div>
                     </div>
+                    <p className="pModal">
+                      <button
+                        type="button"
+                        className="btn btn-secondary d-none"
+                        id="close-modal"
+                        data-dismiss="modal"
+                      >
+                        Close
+                      </button>
+                      Please wait while we verify your account
+                    </p>
                   </div>
-                  <p className="pModal">
-                    <button
-                      type="button"
-                      className="btn btn-secondary d-none"
-                      id="close-modal"
-                      data-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                    Please wait while we verify your account
-                  </p>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <div
+        className="modal fade"
+        id="exampleModalCenter1"
+        tabIndex={-1}
+        role="dialog"
+        aria-labelledby="exampleModalCenterTitle"
+        aria-hidden="true"
+      >
+        <div
+          className="modal-dialog modal-dialog-centered modal-lg"
+          role="document"
+          style={{ padding: "0px" }}
+        >
+          <div className="modal-content modalPic">
+            <div className="container conPic" style={{ padding: "0px" }}>
+              <div className="row" style={{ padding: "0px" }}>
+                <div className="col-lg-12" style={{ padding: "0px" }}>
+                  <img
+                    src=""
+                    className="img-fluid mx-auto d-flex imgHide imgClientpic"
+                  ></img>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
